82fe612fe1df9f28270422d8a41152209be3a6bc
nikhilzambare24 <119861658+nikhilzambare24@users.noreply.github.com>
2023-04-17 11:36:47 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ package Demorepo;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello JAVA..");
+        System.out.println("Hello JAVA...........................................");
     }
 }
